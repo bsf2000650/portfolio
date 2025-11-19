7062c0edc5d7b7d913896f7f41ff57f2b6576dc9
asad <bsf2000650@ue.edu.pk>
2025-11-19 12:45:38 +0500

    Upgraded react-parallax-tilt to compatible with react-19 types

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -4,7 +4,7 @@ import { projects } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 import { styles } from "../styles";
 import { motion } from "framer-motion";
-import { Tilt } from "react-tilt";
+import Tilt from "react-parallax-tilt";
 
 const ProjectCard = ({
   index,
